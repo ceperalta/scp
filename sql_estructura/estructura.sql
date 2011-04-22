c6af324fc540271cd58962f0b8c1cda85d794bea
charly <ceperalta@gmail.com>
2011-04-22 16:34:17 -0300

    ahora test sql3

--- sql_estructura/estructura.sql
+++ sql_estructura/estructura.sql
@@ -434,4 +434,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-04-22 19:33:50
+-- Dump completed on 2011-04-22 19:33:58
