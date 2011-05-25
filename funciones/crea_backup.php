fac1e0f35fbe483660b053666f3ecc07f7ca0f63
charly <ceperalta@gmail.com>
2011-05-25 20:57:17 -0300

    tag

--- funciones/crea_backup.php
+++ funciones/crea_backup.php
@@ -14,13 +14,13 @@ include("../funciones/funciones.php");
 	$minutos = date('i');
 	$segundos = date('s');
 	$fecha_hora = date('j-n-y_').$hora_menos_uno.":".$minutos.":".$segundos."hs";
+	$fecha_hora = str_replace(":","-",$fecha_hora);
 
 $nombre_backup = "scp_v".$version."_".$fecha_hora;
 
 $path_y_nombre_sql = $_SESSION[configuracion][PATH_BASE_FS]."\\temporal\\".$nombre_backup.".sql";
 
-$cmd = $_SESSION[configuracion]PATH_BASE_BIN_MYSQL]."\\mysqldump.exe scp --user=root > " . $path_y_nombre_sql;
-echo $cmd;
+$cmd = $_SESSION[configuracion][PATH_BASE_BIN_MYSQL]."\\mysqldump.exe ".NOMBRE_BD." --user=".USUARIO_BD." -p".CLAVE_BD." > " . $path_y_nombre_sql;
 exec($cmd);
 
 		
@@ -31,14 +31,16 @@ if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
    exit("No se puede abrir el archivo a comprimir <$filename>\n");
 }
 
-$zip->addFile($path_y_nombre_sql,"a.sql");
+$zip->addFile($path_y_nombre_sql,"datos_y_estructura.sql");
 $zip->close();
 
 		
 unlink($path_y_nombre_sql);
 
+if(!file_exists($_SESSION[configuracion][CARPETA_DE_BACKUP]))
+	mkdir($_SESSION[configuracion][CARPETA_DE_BACKUP]);
 
-copy($filename,CARPETA_DE_BACKUP."\\".$nombre_backup.".zip");
+copy($filename,$_SESSION[configuracion][CARPETA_DE_BACKUP]."\\".$nombre_backup.".zip");
 unlink($filename);
 
 	$sql = "update backup_log set realizado='".$nombre_backup.".zip' where id=1";
