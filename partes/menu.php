4e01dadeb8b05c9344445268662a2551f478a4b0
charly <ceperalta@gmail.com>
2011-06-01 21:50:35 -0300

    marca titulo como prueba

--- partes/menu.php
+++ partes/menu.php
@@ -3,7 +3,7 @@
 	include("../configuracion/configuracion.php");
 	include("../funciones/funciones.php");
 
-	echo "SCP  (".$_SESSION[configuracion][ULTIMO_TAG].")";
+	echo "SCP-Pruebas  (".$_SESSION[configuracion][ULTIMO_TAG].")";
 	echo "<hr/>";
 ?>
 
