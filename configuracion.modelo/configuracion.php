5b50d9278cc98cffca4b02af81059bfdf809d36c
charly <ceperalta@gmail.com>
2011-06-08 20:43:54 -0300

    conf mods...

---
+++ configuracion.modelo/configuracion.php
@@ -0,0 +1,8 @@
+<?
+	define(NOMBRE_BD,"scp-real");
+	define(USUARIO_BD,"root");
+	define(CLAVE_BD,"moversa");
+	define(SERVIDOR_BD,"localhost");
+	//
+	define(VERSION_TITULO,"SCP-Desarrollo  (v0.6)");
+?>
